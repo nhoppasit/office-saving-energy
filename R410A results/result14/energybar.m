f5e7f5e47d867df3f8fd39c0edaf4677c01b8324
Nhoppasit <dev.theprogram@gmail.com>
2018-09-06 22:22:53 +0700

    คำนวณ hourly electrical energy

--- R410A results/result14/energybar.m
+++ R410A results/result14/energybar.m
@@ -1,9 +1,11 @@
 function [dEnergy] = energybar(mat_file,datetext)
 
 load(mat_file)
-hourly_energy = max(reshape(EATNetKwh-EATNetKwh(1),60,24));
+eng = EATNetKwh(1:1080);
+hourly_energy = max(reshape(eng-eng(1),60,18));
 dEnergy = diff(hourly_energy);
-bar(dEnergy)
+t = round(hhminsec(1)*24):round(hhminsec(1)*24)+length(dEnergy)-1;
+bar(t,dEnergy)
 set(gca,'xlim',[0.5,23.5],'fontsize',14)
 xlabel('Number of hours')
 ylabel('Hourly electrical energy, Kwh')
