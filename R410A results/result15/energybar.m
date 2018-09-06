c5d7ae695479c15eda1884bf2259ff747594c090
Nhoppasit <dev.theprogram@gmail.com>
2018-09-06 22:43:11 +0700

    สร้าง eng_15

---
+++ R410A results/result15/energybar.m
@@ -0,0 +1,13 @@
+function [dEnergy] = energybar(mat_file,datetext)
+
+load(mat_file)
+eng = EATNetKwh(1:1080);
+hourly_energy = max(reshape(eng-eng(1),60,18));
+dEnergy = diff(hourly_energy);
+t = round(hhminsec(1)*24):round(hhminsec(1)*24)+length(dEnergy)-1;
+bar(t,dEnergy)
+set(gca,'xlim',[0.5,23.5],'fontsize',14)
+xlabel('Number of hours')
+ylabel('Hourly electrical energy, Kwh')
+title(['Hourly Electrical Energy / ' datetext])
+set(gca,'ygrid','on','xgrid','off')
