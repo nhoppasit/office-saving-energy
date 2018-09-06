de532193688223c2d849cb9f9c88376bfef1717c
Nhoppasit <dev.theprogram@gmail.com>
2018-09-06 22:06:35 +0700

    คำนวณ ENG_14

--- R410A results/result14/show_graph.m
+++ R410A results/result14/show_graph.m
@@ -7,15 +7,18 @@ load(en_file)
 t = hhminsec*24;
 power = PTKw;
 energy = EATNetKwh;
-TimeIn = TimeIN;
-TempIn = TempIN;
-RHIn = RHIN;
+TimeIn = TimeIN_WALL;
+TempIn = TempIN_WALL;
+RHIn = RHIN_WALL;
+TimeIn2 = TimeIN_CEN;
+TempIn2 = TempIN_CEN;
+RHIn2 = RHIN_CEN;
 TimeOut = TimeOUT;
 TempOut = TempOUT;
 RHOut = RHOUT;
 %% แสดงกราฟเวลา-กระแส และเวลา-KWh
 figure;
-subplot(311)
+subplot(411)
 hold on
 [ax,h1,h2] = plotyy(t,power,t,energy-energy(1));
 grid on
@@ -44,7 +47,7 @@ pkcnt = length(w);
 title(['Power vs Time & Energy vs Time (ON-OFF = ' num2str(pkcnt) ' times) / ' datetext])
 
 %% Pulse width vs time
-subplot(312)
+subplot(412)
 bar((initcross+finalcross)/2,w*60);
 set(gca,'fontsize',fs);
 axis([0,24,0,onpk])
@@ -52,11 +55,11 @@ grid on
 title(['Power Transmission Period vs Time / ' datetext])
 xlabel('Time, h')
 ylabel('Power ON Time, min')
-text(1,onpk/2+3,['Comp. ON: ' num2str(round(sum(w*60))+1) ' min'],'fontsize',14,'background','white')
+text(1,onpk/2+onpk/4,['Comp. ON: ' num2str(round(sum(w*60))+1) ' min'],'fontsize',14,'background','white')
 text(1,onpk/2,['Comp. OFF: ' num2str(round((t(end)-t(1))*60 - sum(w*60))) ' min'],'fontsize',14,'background','white')
 
 %% Temperature & Humidity Inside
-subplot(313)
+subplot(413)
 [ax,h1,h2] = plotyy(TimeIn*24,TempIn,TimeIn*24,RHIn);
 set(ax(1),'fontsize',fs)
 set(ax(2),'fontsize',fs)
@@ -76,7 +79,38 @@ plot(ax(1),TimeOut*24,TempOut,'r-.','linewidth',2)
 hold(ax(2),'on')
 plot(ax(2),TimeOut*24,RHOut,'b-.','linewidth',2)
 
-title(['Temperature (Red) and Humidity (Blue) vs Time (Inside - solid line, Outside - dash line) / ' datetext])
+title(['WALL Temperature (Red) and Humidity (Blue) vs Time (Inside - solid line, Outside - dash line) / ' datetext])
+
+set(ax(1),'xlim',[0,24])
+set(ax(2),'xlim',[0,24])
+
+set(ax(1),'ylim',[22,40])
+set(ax(1), 'YTickMode', 'auto', 'YTickLabelMode', 'auto')
+set(ax(2),'ylim',[40,100])
+set(ax(2), 'YTickMode', 'auto', 'YTickLabelMode', 'auto')
+
+%% Temperature & Humidity Inside
+subplot(414)
+[ax,h1,h2] = plotyy(TimeIn2*24,TempIn2,TimeIn2*24,RHIn2);
+set(ax(1),'fontsize',fs)
+set(ax(2),'fontsize',fs)
+grid on
+xlabel(ax(1),'Time, h')
+xlabel(ax(2),'Time, h')
+ylabel(ax(1),'Temperature, C')
+ylabel(ax(2),'Humidity, %RH')
+set(ax(1),'ycolor','r')
+set(ax(2),'ycolor','b')
+set(h1,'color','red','linewidth',2)
+set(h2,'color','b','linewidth',2)
+
+%% Temperature & Humidity Outside
+hold(ax(1),'on')
+plot(ax(1),TimeOut*24,TempOut,'r-.','linewidth',2)
+hold(ax(2),'on')
+plot(ax(2),TimeOut*24,RHOut,'b-.','linewidth',2)
+
+title(['CENTRAL Temperature (Red) and Humidity (Blue) vs Time (Inside - solid line, Outside - dash line) / ' datetext])
 
 set(ax(1),'xlim',[0,24])
 set(ax(2),'xlim',[0,24])
